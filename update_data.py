59a9ce3116554d8b1133189a400e9240a5e7e6dd
donuts-test <148853107+donuts-test@users.noreply.github.com>
2024-01-11 11:11:52 -0500

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -1,1 +1,17 @@
+import yaml
+from datetime import datetime
+
+def create_yaml_with_datetime(filename):
+    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    data = {"date_and_time": current_datetime}
+
+    with open(filename, 'w') as file:
+        yaml.dump(data, file, default_flow_style=False)
+
+    print(f"Date and time written to {filename}")
+
+filename = 'datetime.yaml'
+create_yaml_with_datetime(filename)
+
 
