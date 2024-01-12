f7a54a85bec7355c3b342ee025898767cd92521b
donuts-test <148853107+donuts-test@users.noreply.github.com>
2024-01-12 11:40:10 -0500

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -4,7 +4,7 @@ import yaml
 sheet_id = "1GhfX1QMrokc2cWaUUMfuTRBlbEmYqf2gLgEB-oobCBM"
 sheet_name = "news"
 news_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-news = pd.read_csv(url)
+news = pd.read_csv(news_url)
 
 
 data_dict = news.to_dict(orient='records')
